a1ae24277a5c203e424e544496e12b1f289e6988
AndrewUAV <145654146+AndrewUAV@users.noreply.github.com>
2024-08-07 22:36:20 +0300

    Update delete_photo.py

--- delete_photo.py
+++ delete_photo.py
@@ -2,13 +2,13 @@ import os
 
 def delete_every_third_photo(folder_path):
     try:
-        # Получаем список файлов в указанной директории
+     
         files = os.listdir(folder_path)
 
-        # Определяем, какие файлы являются изображениями
+      
         image_files = [file for file in files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
 
-        # Удаляем каждую третью фотографию
+   
         for i in range(2, len(image_files), 3):
             file_to_delete = os.path.join(folder_path, image_files[i])
             os.remove(file_to_delete)
@@ -19,6 +19,5 @@ def delete_every_third_photo(folder_path):
     except Exception as e:
         print(f'Произошла ошибка: {e}')
 
-# Укажите путь к вашей папке с фотографиями
 folder_path = 'data_for_learn/photo_from_video'
 delete_every_third_photo(folder_path)
